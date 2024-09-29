f7e2505ffd1be732de546f824ee5f8a2c119325d
Mykhailo Petrov <mykhailo.petrov@schindler.com>
2024-09-30 02:10:49 +0300

    Add post repository

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,11 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './post.entity';
+import { PostRepository } from './post.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, PostRepository],
 })
 export class PostsModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { GetPostDto } from './dto/get-posts.dto';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
-
+import { PostRepository } from './post.repository';
 @Injectable()
 export class PostsService {
+  constructor(private readonly postRepository: PostRepository) {}
+
   getPosts(getPostDto: GetPostDto) {
-    return 'all posts';
+    return this.postRepository.find();
   }
 
   getPost(id: string) {
